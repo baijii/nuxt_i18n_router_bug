68f33a667347e9bdd7a34e8022f512652cf429e4
baijii <jpeg4000@gmail.com>
2024-04-29 09:31:54 +0200

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/i18n"],
+  i18n: {
+    locales: ['en', 'de'],
+    defaultLocale: 'en',
+    strategy: 'prefix',
+    detectBrowserLanguage: {
+      useCookie: true,
+    }
+  }
+})
---
+++ pages/about-us.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineI18nRoute({
+  paths: {
+    en: '/about-us',
+    de: '/ueber-uns',
+  }
+})
+
+const {locale} = useI18n()
+
+const navLocale = computed(() => navigator.language)
+
+</script>
+
+<template>
+  <h1>About us</h1>
+
+  <h2>useI18n().locale</h2>
+  <p>{{ locale }}</p>
+
+  <h2>navigator.language</h2>
+  <p>{{ navLocale }}</p>
+</template>
